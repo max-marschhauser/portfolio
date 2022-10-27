6be99c99fc1b3e6de774cd15be10f6822474a6e0
max-marschhauser <max.marschhauser@gmail.com>
2022-10-27 17:03:47 +0200

    setting up files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,3 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./layout/layout/Layout";
-import Home from "./pages/home/Home";
-import NoPage from "./pages/noPage/NoPage";
-import "./App.scss";
-
 export default function App() {
-	return (
-		<>
-			<div>123</div>
-		</>
-	);
+	return <div className="App">Hello world!</div>;
 }
