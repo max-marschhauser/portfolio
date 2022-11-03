e1ffba662c48266ace9f63873c06dc30f4244e10
max-marschhauser <105870867+max-marschhauser@users.noreply.github.com>
2022-11-03 09:16:05 +0100

    updating footer

--- src/layout/footer/Footer.js
+++ src/layout/footer/Footer.js
@@ -1,6 +1,57 @@
 import React from "react";
 import "./footer.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Footer() {
-	return <div>Footer</div>;
+	return (
+		<footer className="pageFooter">
+			<div className="pageFooter__item">
+				<section>
+					<h2>
+						Max Marschhauser
+						<br />
+						<span>
+							max.marschhauser
+							<wbr />
+							@gmail.com
+						</span>
+					</h2>
+					<ul className="links--social">
+						<li>
+							<a href="/">
+								<FontAwesomeIcon icon="fa-brands fa-github" />
+							</a>
+						</li>
+						<li>
+							<a href="/">
+								<FontAwesomeIcon icon="fa-brands fa-linkedin-in" />
+							</a>
+						</li>
+					</ul>
+					<nav className="links--pages">
+						<ul>
+							<li>
+								<a href="/">other page 1</a>
+							</li>
+							<li>
+								<a href="/">other page 2</a>
+							</li>
+							<li>
+								<a href="/">other page 3</a>
+							</li>
+						</ul>
+					</nav>
+				</section>
+				<aside>
+					<iframes>google maps</iframes>
+				</aside>
+			</div>
+			<ul>
+				<li>copyright 2022</li>
+				<li>
+					<a href="/">back to top</a>
+				</li>
+			</ul>
+		</footer>
+	);
 }
