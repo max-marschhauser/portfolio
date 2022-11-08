c0b44943f53e6eb511284b06ccf0ebf858fa5339
max-marschhauser <105870867+max-marschhauser@users.noreply.github.com>
2022-11-08 09:43:38 +0100

    Update AnotherPage.js

--- src/pages/anotherPage/AnotherPage.js
+++ src/pages/anotherPage/AnotherPage.js
@@ -1,11 +1,8 @@
 // page is displayed if there is no valid url link
 
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import "./anotherPage.scss";
 
 export default function AnotherPage() {
-	const navigate = useNavigate();
-
 	return <p>Another Page</p>;
 }
