9f5fbd3e591514a7261513b6ea42a164d9d0fa93
max-marschhauser <max.marschhauser@gmail.com>
2022-11-19 17:51:43 +0100

    added skills page descriptions

--- src/pages/data/skillsPageExperiences.js
+++ src/pages/data/skillsPageExperiences.js
@@ -1,17 +1,47 @@
 // contains data which is shown on Skills page / component
 
 let skills = {
-	HTML: ["I love HTML", "My favorite part are forms."],
-	CSS: ["CSS is the best", "I am very good with Flex and Grid", "Even better with new things link has()"],
-	Sass: ["Sass is the best"],
-	JavaScript: ["I like both vanilla JS and it's frameworks / libraries"],
-	TypeScript: ["Makes bugs go away"],
-	React: ["Love on the first sight"],
-	PHP: ["Learnt it in school", "Used it to connect Pages with Databases"],
-	MongoDB: ["Nice noSQL table"],
-	MySQL: ["I like SQL tables", "Learned it in school"],
-	NodeJS: ["Still something foreigh but I'm getting there"],
-	C: ["My first programming language", "Learned it in school"],
+	HTML: [
+		"Started using it in 2010.",
+		"Feel very comfortable with it.",
+		"I usually write semantic HTML with regard to user accessibility.",
+	],
+	CSS: [
+		"Started using it in 2010.",
+		"Very comfortable with newer features like has(), layers and container queries.",
+		"Creating responsive web pages with ease.",
+		"Familiar with CSS frameworks like Bootstrap and Tailwind.",
+		"Love to use Sass (CSS preprocessor).",
+		"I prefer BEM methodology.",
+	],
+	Sass: [
+		"Love to use Sass (CSS preprocessor.",
+		"My favorite features are Nesting, Mixins and Modular CSS.",
+		"I prefer BEM methodology.",
+	],
+	JavaScript: [
+		"First contanct with it was in 2020., but started actively using it in 2022.",
+		"Feel comfortable with both vanilla JavaScript and it's frameworks / libraries.",
+		"I prefer using React for buildung Single Page Applications.",
+		"Gain FCC cetrificate on using JavaScript in web desing.",
+	],
+	TypeScript: [
+		"Started actively using it in 2022.",
+		"Love on the first sight, once i tried using it it almost instantly became my favorite way of writing JavaScript.",
+	],
+	React: ["My prefered way of writing JavaScript.", "Use it to create Single Page Applications with ease."],
+	PHP: [
+		"Learned it in 2012.",
+		"Used it mainly to connect Pages with SQL Databases, and to create interactive web pages before I learned JavaScript.",
+	],
+	MongoDB: ["Used it a couple of times.", "Still not very comfortable with it, would need a little bit of learning."],
+	MySQL: ["Learned it in 2012.", "Not the thing I use every day, would need a little bit of reminding."],
+	NodeJS: ["Still something foreigh to me but I'm getting there."],
+	C: [
+		"My first programming language.",
+		"Used it in school from 2009. to 2013.",
+		"Learned fundamentals of programing with it.",
+	],
 };
 
 export default skills;
