3afd19f9195ddf748ad8ec3c240bf95066823c0f
max-marschhauser <105870867+max-marschhauser@users.noreply.github.com>
2022-11-25 09:22:50 +0100

    general layout of the projects page

--- src/pages/projectsPage/ProjectsPage.js
+++ src/pages/projectsPage/ProjectsPage.js
@@ -6,8 +6,52 @@ import "./projectsPage.scss";
 export default function ProjectsPage() {
 	return (
 		<div className="main">
-			<section className="skillsPage">
+			<section className="projectsPage">
 				<h2 className="pageHeading">Projects</h2>
+				<div className="projectsPage__container">
+					<div className="projectsPage__list">
+						<article className="projectsPage__list__item">
+							<img alt="project1" />
+							<div className="projectsPage__list__item__description">
+								<ul>
+									<li>
+										<h3>Item Name</h3>
+									</li>
+									<li>Technology used</li>
+									<li>Description</li>
+									<li>Link</li>
+								</ul>
+							</div>
+						</article>
+						<article className="projectsPage__list__item">
+							<img alt="project1" />
+							<div className="projectsPage__list__item__description">
+								<ul>
+									<li>
+										<h3>Item Name</h3>
+									</li>
+									<li>Technology used</li>
+									<li>Description</li>
+									<li>Link</li>
+								</ul>
+							</div>
+						</article>
+						<article className="projectsPage__list__item">
+							<img alt="project1" />
+							<div className="projectsPage__list__item__description">
+								<ul>
+									<li>
+										<h3>Item Name</h3>
+									</li>
+									<li>Technology used</li>
+									<li>Description</li>
+									<li>Link</li>
+								</ul>
+							</div>
+						</article>
+					</div>
+					<div className="projectsPage__filter"></div>
+				</div>
 			</section>
 		</div>
 	);
