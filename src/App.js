78f02f2916fcbbced62093cb52af6f9416edfa00
max-marschhauser <105870867+max-marschhauser@users.noreply.github.com>
2022-12-12 16:00:59 +0100

    root documentation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+// main app file
+
+// importing general items
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// importing pages
 import Layout from "./layout/layout/Layout";
 import Home from "./pages/home/Home";
 import SkillsPage from "./pages/skillsPage/SkillsPage";
 import ProjectsPage from "./pages/projectsPage/ProjectsPage";
 import AboutPage from "./pages/aboutPage/AboutPage";
 import NoPage from "./pages/noPage/NoPage";
+
+// importing styles
 import "./assets/styles/styles.scss";
+
+// importing utils
 import ScrollNavlinkToTop from "./utils/scrollNavlinkToTop";
 
 export default function App() {
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// root file
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
