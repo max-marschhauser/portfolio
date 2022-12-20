cf6d194476514a67bad9a528c24d098d19f28a86
max-marschhauser <105870867+max-marschhauser@users.noreply.github.com>
2022-12-20 08:58:35 +0100

    adding about page components

--- src/pages/aboutPage/AboutPage.js
+++ src/pages/aboutPage/AboutPage.js
@@ -3,11 +3,12 @@
 // importing general items
 import React from "react";
 
-// importing data
-import aboutData from "../../data/aboutPageText.js";
-
 // importing styles
 import "./aboutPage.scss";
+
+// importing components
+import RelatedToProgramming from "./components/relatedToProgramming/RelatedToProgramming.js";
+import UnrelatedToProgramming from "./components/unrelatedToProgramming/UnrelatedToProgramming.js";
 
 export default function AboutPage() {
 	return (
@@ -19,21 +20,14 @@ export default function AboutPage() {
 					<h3>Related to programming</h3>
 
 					<div className="aboutPage--contents--articles">
-						<article>
-							<h4>School - Basics</h4>
-							<a href="" target="_blank" rel="noreferrer">
-								<img src={""} alt="" />
-							</a>
-							<ul>
-								<li></li>
-								<li></li>
-							</ul>
-						</article>
+						<RelatedToProgramming />
 					</div>
 				</div>
 				<div className="aboutPage--contents">
 					<h3>Unrelated to programming</h3>
-					<div className="aboutPage--contents--articles"></div>
+					<div className="aboutPage--contents--articles">
+						<UnrelatedToProgramming />
+					</div>
 				</div>
 			</section>
 		</div>
---
+++ src/pages/aboutPage/components/relatedToProgramming/RelatedToProgramming.js
@@ -0,0 +1,20 @@
+// improting general items
+import React from "react";
+
+// importing data
+// import aboutData from "../../../../data/aboutPageText.js";
+
+export default function RelatedToProgramming() {
+	return (
+		<article>
+			<h4>School - Basics</h4>
+			<a href="#home" target="_blank" rel="noreferrer">
+				<img src={""} alt="" />
+			</a>
+			<ul>
+				<li></li>
+				<li></li>
+			</ul>
+		</article>
+	);
+}
