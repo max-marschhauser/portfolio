5a3a788ae9684ed0a089c0b2e2d86cfbffa0eec3
max-marschhauser <105870867+max-marschhauser@users.noreply.github.com>
2022-12-21 11:05:07 +0100

    updating about page contents

--- src/data/aboutPageText.js
+++ src/data/aboutPageText.js
@@ -12,12 +12,12 @@ const aboutData = [
 		imgSrc: undefined,
 		link: "",
 		content: [
-			"Started learning programming in school in 2009. at the age of 14.",
-			"First programming language was C.",
-			"Used it for 4 years, learned programming basics with it.",
-			"2010. started learning web desing (HTML and CSS).",
-			"2012. started using PHP and MySql, mainly for connecting web pages with databases.",
-			"In university attended two courses related to modern web design (2017. and 2018.).",
+			`Started learning programming in school in 2009. at the age of 14.`,
+			`First programming language was C.`,
+			`Used it for 4 years, learned programming basics with it.`,
+			`2010. started learning web desing (HTML and CSS).`,
+			`2012. started using PHP and MySql, mainly for connecting web pages with databases.`,
+			`In university attended two courses related to modern web design (2017. and 2018.).`,
 		],
 	},
 	{
@@ -26,8 +26,8 @@ const aboutData = [
 		imgSrc: javascriptCertificateSrc,
 		link: "https://www.freecodecamp.org/certification/fcc1fd16a03-be4d-47cc-a798-42c2fbc37651/javascript-algorithms-and-data-structures",
 		content: [
-			"JavaScript, TypeScript, React and other more advenced topics I learned on my own, using array of internet sources souch as MDM, w3schools, stackOverflow, and Youtube tutorials.",
-			"Also, finished some online courses such as freeCodeCamp.",
+			`JavaScript, TypeScript, React and other more advenced topics I learned on my own, using array of internet sources souch as MDM, w3schools, stackOverflow, and Youtube tutorials.`,
+			`Also, finished some online courses such as freeCodeCamp.`,
 		],
 	},
 	{
@@ -36,9 +36,9 @@ const aboutData = [
 		imgSrc: githubSrc,
 		link: "https://github.com/max-marschhauser",
 		content: [
-			"Only recently did I start using Git and GitHub to store my code.",
-			"GitHub contributions from the images above show my motivation for programming.",
-			"Every day I try to create something new, to implement some new code.",
+			`Only recently did I start using Git and GitHub to store my code.`,
+			`GitHub contributions from the images above show my motivation for programming.`,
+			`Every day I try to create something new, to implement some new code.`,
 		],
 	},
 	{
@@ -47,11 +47,11 @@ const aboutData = [
 		imgSrc: undefined,
 		link: "",
 		content: [
-			"I was a computer science teacher for 3 years.",
-			"Introducing students with programming.",
-			"Mostly taught beginner level programming with languages such as Python for older students and Scratch for younger ones, also introduction to HMTL and CSS.",
-			"Helped students build their first Applications.",
-			"Also tought about other things related to computer science.",
+			`I was a computer science teacher for 3 years.`,
+			`Introducing students with programming.`,
+			`Mostly taught beginner level programming with languages such as Python for older students and Scratch for younger ones, also introduction to HMTL and CSS.`,
+			`Helped students build their first Applications.`,
+			`Also tought about other things related to computer science.`,
 		],
 	},
 
@@ -61,8 +61,8 @@ const aboutData = [
 		imgSrc: undefined,
 		link: "",
 		content: [
-			"Studied at the III. gymnasium (Mathematics and Natural Sciences) in Osijek (2009. - 2013.).",
-			"Graduated in History and Pedagogy at the Faculty of Philosophy in Osijek (2013. - 2018.).",
+			`Studied at the III. gymnasium (Mathematics and Natural Sciences) in Osijek (2009. - 2013.).`,
+			`Graduated in History and Pedagogy at the Faculty of Philosophy in Osijek (2013. - 2018.).`,
 		],
 	},
 	{
@@ -71,8 +71,8 @@ const aboutData = [
 		imgSrc: undefined,
 		link: "",
 		content: [
-			"4 years as a school counselor in elementary school, gained leadership experience and team work related problem solving experience from leading a team of about 50 people.",
-			"3 years as a computer science teacher in the same school, where I even more improved my computer science knowledge.",
+			`4 years as a school counselor in elementary school, gained leadership experience and team work related problem solving experience from leading a team of about 50 people.`,
+			`3 years as a computer science teacher in the same school, where I even more improved my computer science knowledge.`,
 		],
 	},
 	{
@@ -81,11 +81,11 @@ const aboutData = [
 		imgSrc: undefined,
 		link: "",
 		content: [
-			"Very responsible.",
-			"Good workflow both in a team and individualy.",
-			"Good Soft Skills such as interpersonal skills, time menagement, problem-solving oriented, empathy...",
-			"Advanced Comunication and Listening skills.",
-			"Have will to learn and advance for the benefit of both the company and myself.",
+			`Very responsible.`,
+			`Good workflow both in a team and individualy.`,
+			`Good Soft Skills such as interpersonal skills, time menagement, problem-solving oriented, empathy...`,
+			`Advanced Comunication and Listening skills.`,
+			`Have will to learn and advance for the benefit of both the company and myself.`,
 		],
 	},
 	{
@@ -94,10 +94,10 @@ const aboutData = [
 		imgSrc: undefined,
 		link: "",
 		content: [
-			"Feel very comfortable with using English languange in both the speaking and writing form.",
-			"Know the basics of German language but didn't use it for a few years, would need some catch-up to be completely comfortable with using it.",
-			"Worked in a Hungarian school and learned Hungarian language to the B1 level, could use it in a simpler conversation.",
-			"Driving licence B category (private car).",
+			`Feel very comfortable with using English languange in both the speaking and writing form.`,
+			`Know the basics of German language but didn't use it for a few years, would need some catch-up to be completely comfortable with using it.`,
+			`Worked in a Hungarian school and learned Hungarian language to the B1 level, could use it in a simpler conversation.`,
+			`Driving licence B category (private car).`,
 		],
 	},
 ];
