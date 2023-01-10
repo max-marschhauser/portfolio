288c1a040439c1911b25fb33cb911dea526e3c75
max-marschhauser <105870867+max-marschhauser@users.noreply.github.com>
2023-01-10 11:40:45 +0100

    added hamburger button for mobile navbar

--- src/layout/layout/Layout.js
+++ src/layout/layout/Layout.js
@@ -12,43 +12,46 @@ import "./layout.scss";
 export default function Layout() {
 	return (
 		<>
-			<div className="pageContainer">
-				<aside className="aside">
-					<div className="logo">
-						<span>MAX</span>
-						<span>Marschhauser</span>
-						<span>
-							React Frontend <wbr />
-							Developer
-						</span>
-					</div>
-					<nav id="top">
-						<ul>
-							<li>
-								<NavLink className="navbar__link" to="/" end>
-									Home
-								</NavLink>
-							</li>
-							<li>
-								<NavLink className="navbar__link" to="/skills">
-									Skills
-								</NavLink>
-							</li>
-							<li>
-								<NavLink className="navbar__link" to="/projects">
-									Projects
-								</NavLink>
-							</li>
-							<li>
-								<NavLink className="navbar__link" to="/about">
-									About
-								</NavLink>
-							</li>
-						</ul>
-					</nav>
-				</aside>
-				<Outlet />
+			<div className="navbar">
+				<div className="logo">
+					<span>MAX</span>
+					<span>Marschhauser</span>
+					<span>
+						React Frontend <wbr />
+						Developer
+					</span>
+				</div>
+				<button className="hamburgerButton">
+					<span></span>
+					<span></span>
+					<span></span>
+				</button>
+				<nav id="top">
+					<ul>
+						<li>
+							<NavLink className="navbar__link" to="/" end>
+								Home
+							</NavLink>
+						</li>
+						<li>
+							<NavLink className="navbar__link" to="/skills">
+								Skills
+							</NavLink>
+						</li>
+						<li>
+							<NavLink className="navbar__link" to="/projects">
+								Projects
+							</NavLink>
+						</li>
+						<li>
+							<NavLink className="navbar__link" to="/about">
+								About
+							</NavLink>
+						</li>
+					</ul>
+				</nav>
 			</div>
+			<Outlet />
 			<Footer />
 		</>
 	);
