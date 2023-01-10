f9aab4ee6d534b5ef0c8dce95eeaa393f6d0cb13
max-marschhauser <105870867+max-marschhauser@users.noreply.github.com>
2023-01-10 08:56:25 +0100

    changes to home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -12,21 +12,21 @@ import About from "../aboutPage/AboutPage";
 import "./home.scss";
 
 // importing images
-import ReactImage from "../../assets/images/skills/React.png";
 import bearImage from "../../assets/images/Bear.jpg";
 
 export default function Home() {
 	return (
 		<div className="main">
 			<section className="homePage">
-				<div className="nameAndImage">
-					<h1>MAX MARSCHHAUSER</h1>
-					<img src={bearImage} alt="max-marschhauser-profile" />
+				<div className="homePage--container">
+					<div className="nameAndImage">
+						<img src={bearImage} alt="max-marschhauser-profile" />
+						<h1>MAX MARSCHHAUSER</h1>
+					</div>
+					<h2>
+						<span>&lt;</span> <span>React Frontend Developer</span> <span>&gt;</span>
+					</h2>
 				</div>
-				<h2>
-					<span>&lt;</span> <span>React Frontend Developer</span> <span>&gt;</span>
-				</h2>
-				<img className="reactImage" src={ReactImage} alt="react-logo" />
 			</section>
 			<section className="blankSection"></section>
 			<Skills />
