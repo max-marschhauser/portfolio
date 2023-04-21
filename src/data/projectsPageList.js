66484128c6ee6158b21c859448d8c73307604aaf
Max Marschhauser <max.marschhauser@gmail.com>
2023-04-21 10:02:46 +0200

    fixed data file

--- src/data/projectsPageList.js
+++ src/data/projectsPageList.js
@@ -125,7 +125,7 @@ const projectsData = [
 		description:
 			"A digital store built using JavaScript, HTML and SCSS. Responsive eCommerce Single-page app. It can also be used as a brochure page for business. Data is stored in an object file and imported when needed. User can filter or sort displayed items and add items to the cart.",
 		githubLink: "https://github.com/max-marschhauser/Digital-Store",
-		codepenLink: "",
+		codepenLink: "https://codepen.io/max-marschhauser/pen/GRYNzNB",
 	},
 	{
 		id: 2,
